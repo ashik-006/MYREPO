6491912d73162bf98a7d2bc9cb67b6a14c8710ed
Smart <Smart@LAPTOP-RVKJ6BBN>
2024-07-15 08:29:34 +0530

    Assignment

---
+++ Mymavenproject/src/main/java/Assignments/Assignment_3.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+public class Assignment_3 
+{
+	static int  age=25;
+	public static   int voting(int age)
+	{
+		return age;
+	}
+	public static void main(String[] args) 
+{
+	if(age>18)
+	{
+		System.out.println("ELIGIBLE FOR VOTING");
+	}
+	else
+	{
+		System.out.println("NOT ELIGIBLE FOR VOTING");
+	}
+}
+	
+}
